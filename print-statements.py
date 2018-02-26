049de44313d0d00c0240faa232f325a68f4fe3cc
cris <crispope@gmail.com>
2018-02-25 17:24:56 -0800

    practice print statements

---
+++ print-statements.py
@@ -0,0 +1,8 @@
+print("Hello Python")
+print("Hi", "Python")
+
+#Using a separator
+print("Hello", "Python", sep="-")
+
+#New style string format of Py3 using {} as a place holder for the value being passed in to the format function.
+print("Hi {}".format("Python"))
